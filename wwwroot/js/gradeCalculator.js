c05d6c185ccda3d317ef77d428fc37bea2e1a0de
Logan-Hardy <70916374+Logan-Hardy@users.noreply.github.com>
2021-01-18 11:11:53 -0700

    Add project files.

---
+++ wwwroot/js/gradeCalculator.js
@@ -0,0 +1,9 @@
+﻿var submit = document.getElementById("submitButton");
+
+submit.addEventListener("click", function () {
+    alert("Assignments: " + document.getElementById("assignments").value + "\n" +
+        "Group Project: " + document.getElementById("groupProject").value + "\n" +
+        "Quizzes: " + document.getElementById("quizzes").value + "\n" +
+        "Exams: " + document.getElementById("exams").value + "\n" +
+        "INTEX: " + document.getElementById("intex").value);
+})
